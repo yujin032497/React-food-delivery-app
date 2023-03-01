7f216b6e9b1b4ee6b413a43a65147552da52b1e9
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2023-03-02 03:55:28 +0900

    FIX 2:02:35

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Container } from "reactstrap";
 import logo from "../../assets/images/res-logo.png";
 import { NavLink, Link } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+
+import "../../styles/header.css";
 
 const nav__links = [
     {
@@ -24,31 +27,56 @@ const nav__links = [
 ]
 
 const Header = () => {
-    return <header classname="header">
 
-    <Container>
-        <div className="nav__wrapper">
-            <div className="logo">
-                <img src={logo} alt="logo" />
-                <h5>Tasty Treat</h5>
-            </div>
+    const menuRef = useRef(null)
+    const toggleMenu = () => menuRef.current.classList.toggle("show__menu")
+    return (
+        <header className="header">
+            <Container>
+                <div className="nav__wrapper d-flex align-items-center justify-content-between">
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                        <h5>Tasty Treat</h5>
+                    </div>
 
-            {/* ======= menu ======= */}
-            <div className="navigation">
-                <div className="menu">
-                {
-                    nav__links.map((item, index) => (
-                    <NavLink to={item.path} key={index}>
-                        {item.display}
-                    </NavLink>
-                ))}
+                    {/* ======= menu ======= */}
+                    <div className="navigation" ref={ menuRef } onClick={ toggleMenu }>
+                        <div className="menu d-flex align-items-center gap-5">
+                        {nav__links.map((item, index) => (
+                            <NavLink 
+                                to={item.path} 
+                                key={index}
+                                className={(navClass) =>
+                                    navClass.isActive ? "active__menu" : ""
+                                }
+                            >
+                                {item.display}
+                            </NavLink>
+                        ))}
+                        </div>
+                    </div>
+                    
+                    {/* ======= nav right icons ======= */}
+                    <div className="nav__right d-flex align-items-center gap-4">
+                        <span className="cart__icon">
+                            <i class="ri-shopping-basket-line"></i>
+                            <span className="cart__badge">2</span>
+                        </span>
+
+                        <span className="user">
+                            <Link to="/login">
+                                <i class="ri-user-line"></i>
+                            </Link>
+                        </span>
+
+                        <span className="mobile__menu" onClick={ toggleMenu }>
+                            <i class="ri-menu-line"></i>
+                        </span>
+                    </div>
                 </div>
-            </div>
-
-        </div>
-    </Container>
-
-    </header>
+            </Container>
+        </header>
+    );
 }
 
 export default Header;
